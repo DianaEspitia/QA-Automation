4864128855a3d9a12034c37aa1ca45adb653a234
DianaEspitia <78936469+DianaEspitia@users.noreply.github.com>
2021-11-25 13:17:55 -0500

    Update mercado_libre.py

--- mercado_libre.py
+++ mercado_libre.py
@@ -2,16 +2,16 @@ from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import time
 
-buscador = 1
+buscador = 1 #Seleccionar el buscador
 
 if buscador == 1:
-    driver = webdriver.Chrome('C:/Users/DianaEspitiaTorres/Downloads/chromedriver/chromedriver.exe')
+    driver = webdriver.Chrome('C:/Users/DianaEspitiaTorres/Downloads/chromedriver/chromedriver.exe') 
 elif buscador ==2:
     driver = webdriver.Firefox()
 
-driver.get("https://www.mercadolibre.com.co/") # Abrir Google
+driver.get("https://www.mercadolibre.com.co/") #Abrir página de mercado libre
 
-barra = driver.find_element_by_xpath('/html/body/header/div/form/input')
-barra.send_keys('Kit Lettering')
-barra.send_keys(Keys.ENTER)
+barra = driver.find_element_by_xpath('/html/body/header/div/form/input') #Identificar el elemento (barra de búsqueda)
+barra.send_keys('Kit Lettering') #Artículos que deseamos buscar
+barra.send_keys(Keys.ENTER) #Enter para buscar 
 time.sleep(2)
