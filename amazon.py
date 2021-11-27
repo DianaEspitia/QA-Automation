5774a713d695892d1f8a045d841bcbd464389807
DianaEspitia <78936469+DianaEspitia@users.noreply.github.com>
2021-11-27 16:26:45 -0500

    Update amazon.py

--- amazon.py
+++ amazon.py
@@ -2,23 +2,18 @@ from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import time
 
-buscador = 1
-
-if buscador == 1:
-    driver = webdriver.Chrome('C:/Users/DianaEspitiaTorres/Downloads/chromedriver/chromedriver.exe')
-elif buscador == 2:
-    driver = webdriver.Firefox()
+driver = webdriver.Chrome('C:/Users/DianaEspitiaTorres/Downloads/chromedriver/chromedriver.exe')
 
 driver.get('https://www.amazon.com/-/es/')
 
 barra = driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]')
-barra.send_keys('kit lettering')
+barra.send_keys('ropa nasa')
 barra.send_keys(Keys.ENTER)
-
-articulo = driver.find_element_by_xpath('//*[@id="search"]/div[1]/div[1]/div/span[3]/div[2]/div[13]/div/span/div/div/div[2]/div[1]/h2')
+                                         
+articulo = driver.find_element_by_xpath('//*[@id="search"]/div[1]/div[1]/div/span[3]/div[2]/div[2]/div/span/div/div/div/div/span/a/div/img')
 articulo.click()
 
-agregar = driver.find_element_by_xpath('//*[@id="add-to-cart-button"]')
+agregar = driver.find_element_by_xpath('//*[@id="add-to-cart-button"]') 
 agregar.click()
 
 carrito = driver.find_element_by_xpath('//*[@id="hlb-view-cart-announce"]')
